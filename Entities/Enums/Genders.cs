a69b6bee28530b9ade01229b7f39a23956eb8730
aliyasindogan <a.yasindogan@gmail.com>
2021-07-06 14:28:56 +0300

    Entities Concrete Enums da Genders oluşturuldu.

---
+++ Entities/Enums/Genders.cs
@@ -0,0 +1,15 @@
+﻿namespace Entities.Enums
+{
+    public enum Genders
+    {
+        /// <summary>
+        /// Kadın
+        /// </summary>
+        Famele = 0,
+
+        /// <summary>
+        /// Erkek
+        /// </summary>
+        Male = 1
+    }
+}
