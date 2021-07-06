7767e022243da579982a62dd5ceacee26ce1a06a
aliyasindogan <a.yasindogan@gmail.com>
2021-07-06 14:24:01 +0300

    Entities Abstract da ICreatedEntity oluşturuldu.

---
+++ Entities/Abstract/ICreatedEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Entities.Abstract
+{
+    public interface ICreatedEntity
+    {
+        int CreatedUserId { get; set; }
+        DateTime CreatedDate { get; set; }
+    }
+}
