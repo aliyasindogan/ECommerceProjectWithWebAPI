e2e85e18399e7fb7a27c0afcf5769d0563430d9f
aliyasindogan <a.yasindogan@gmail.com>
2021-07-06 14:25:24 +0300

    Entities Abstract da IUpdatedEntity oluşturuldu.

---
+++ Entities/Abstract/IUpdatedEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Entities.Abstract
+{
+    public interface IUpdatedEntity
+    {
+        int? UpdatedUserId { get; set; }
+        DateTime? UpdatedDate { get; set; }
+    }
+}
