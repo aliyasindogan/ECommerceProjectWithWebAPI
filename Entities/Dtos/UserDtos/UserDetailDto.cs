0ca103682fe8078b2e4cf3f5e4dc4504762aa6ac
aliyasindogan <a.yasindogan@gmail.com>
2021-07-06 14:27:30 +0300

    Entities Concrete Dtos de UserDetailDto oluşturuldu.

---
+++ Entities/Dtos/UserDtos/UserDetailDto.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+
+namespace Entities.Dtos.UserDtos
+{
+    public class UserDetailDto : IDto
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
