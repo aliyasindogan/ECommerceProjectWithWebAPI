9025396deb78e98db4db06c5cc1f04776ac8f251
aliyasindogan <a.yasindogan@gmail.com>
2021-07-06 14:26:12 +0300

    Entities Concrete dw AuditableEntity oluşturuldu.

---
+++ Entities/Concrete/BaseEntities/AuditableEntity.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Abstract;
+using System;
+
+namespace Entities.Concrete.BaseEntities
+{
+    public class AuditableEntity : BaseEntity, ICreatedEntity, IUpdatedEntity
+    {
+        public int CreatedUserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int? UpdatedUserId { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
