cd972dd7777d42b69f1977737ee72a693af9b5d8
aliyasindogan <a.yasindogan@gmail.com>
2021-07-06 14:24:57 +0300

    Entities Abstract da IEntity oluşturuldu.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    /// <summary>
+    /// Veritabanında karşılık gelen tablolarda olacak
+    /// </summary>
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
