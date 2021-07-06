d47f55dec5870dc7cbf5c6b0d1b615faadd65547
aliyasindogan <a.yasindogan@gmail.com>
2021-07-06 14:26:37 +0300

    Entities Concrete de User oluşturuldu.

---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Entities.Concrete.BaseEntities;
+
+namespace Entities.Concrete
+{
+    public class User : AuditableEntity
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public bool Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
