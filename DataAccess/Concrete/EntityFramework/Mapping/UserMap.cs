07702329fc0957ac1d8c7a099000eb69a26a985a
aliyasindogan <a.yasindogan@gmail.com>
2021-07-11 21:55:07 +0300

    DataAccess de UserMap oluşturuldu.

---
+++ DataAccess/Concrete/EntityFramework/Mapping/UserMap.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Concrete.EntityFramework.Mapping
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users", @"dbo");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.UserName)
+                .HasColumnName("UserName")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.FirstName)
+                .HasColumnName("FirstName")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.LastName)
+                .HasColumnName("LastName")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.Password)
+                .HasColumnName("Password")
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(x => x.Gender)
+                .HasColumnName("Gender")
+                .IsRequired();
+
+            builder.Property(x => x.DateOfBirth)
+                .HasColumnName("DateOfBirth")
+                .IsRequired();
+            builder.Property(x => x.CreatedDate).HasDefaultValue(DateTime.Now);
+
+            builder.HasData(new User
+            {
+                Id = 1,
+                FirstName = "Ali Yasin",
+                LastName = "Doğan",
+                Password = "12345",
+                Gender = true,
+                DateOfBirth = Convert.ToDateTime("01-01-1979"),
+                CreatedDate = DateTime.Now,
+                Address = "İSTANBUL",
+                CreatedUserId = 1,
+                Email = "ali@gmail.com",
+                UserName = "aliyasin"
+            });
+        }
+    }
+}
