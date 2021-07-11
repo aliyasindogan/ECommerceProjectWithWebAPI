c23e11e5f2231fb8a8ad1de3d422ab950ad70e8b
aliyasindogan <a.yasindogan@gmail.com>
2021-07-11 21:54:45 +0300

    DataAccess de ECommerceProjectWithWebAPIContext oluşturuldu.

---
+++ DataAccess/Concrete/Contexts/ECommerceProjectWithWebAPIContext.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Concrete.EntityFramework.Mapping;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.Contexts
+{
+    public class ECommerceProjectWithWebAPIContext : DbContext
+    {
+        public ECommerceProjectWithWebAPIContext(DbContextOptions<ECommerceProjectWithWebAPIContext> options) : base(options)
+        {
+        }
+
+        public ECommerceProjectWithWebAPIContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connString = "Data Source =.\\SQLEXPRESS; Initial Catalog = ECommerceProjectWithWebAPIDb; Integrated Security = True";
+            optionsBuilder.UseSqlServer(connString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
