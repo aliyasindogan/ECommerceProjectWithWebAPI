7071ef6dbe7a912fc8520b860e28d16bc1fc79cb
aliyasindogan <a.yasindogan@gmail.com>
2021-07-12 00:22:10 +0300

    UserDetailDto class ına Id property eklendi.Ve password kaldırıldı.

--- Entities/Dtos/UserDtos/UserDetailDto.cs
+++ Entities/Dtos/UserDtos/UserDetailDto.cs
@@ -5,10 +5,10 @@ namespace Entities.Dtos.UserDtos
 {
     public class UserDetailDto : IDto
     {
+        public int Id { get; set; }
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Password { get; set; }
         public string Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
