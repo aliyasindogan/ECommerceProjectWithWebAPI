b5142f436f56e1b8ddc3fdff0201fec8b2608ac6
aliyasindogan <a.yasindogan@gmail.com>
2021-07-12 00:21:19 +0300

    Bussines da IUserService oluşturuldu.

---
+++ Bussines/Abstarct/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Dtos.UserDtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Bussines.Abstarct
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDetailDto>> GetListAsync();
+
+        Task<UserDto> GetByIdAsync(int id);
+
+        Task<UserDto> AddAsync(UserAddDto userAddDto);
+
+        Task<UserUpdateDto> UpdateAsync(UserUpdateDto userUpdateDto);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}
