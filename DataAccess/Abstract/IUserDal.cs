af4be147995b66e029d41a3a9596c47a1fdd284f
aliyasindogan <a.yasindogan@gmail.com>
2021-07-11 21:54:27 +0300

    DataAccess de IUserDal ve EfUserDal oluşturuldu.

---
+++ DataAccess/Abstract/IUserDal.cs
@@ -0,0 +1,8 @@
+﻿using Entities.Concrete;
+
+namespace DataAccess.Abstract
+{
+    public interface IUserDal : IBaseRepository<User>
+    {
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,10 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete.Contexts;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfUserDal : EfBaseRepository<User, ECommerceProjectWithWebAPIContext>, IUserDal
+    {
+    }
+}
