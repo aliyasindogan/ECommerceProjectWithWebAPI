e64e75adcf9a9075240839c1eb6bd104672dbbe9
aliyasindogan <a.yasindogan@gmail.com>
2021-07-11 21:53:51 +0300

    DataAccess de IBaseRepository ve EfBaseRepository oluşturuldu.

---
+++ DataAccess/Concrete/EfBaseRepository.cs
@@ -0,0 +1,66 @@
+﻿using Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class EfBaseRepository<TEntity, TContext> : IBaseRepository<TEntity>
+        where TEntity : class, IEntity, new()
+    where TContext : DbContext, new()
+    {
+        public async Task<IEnumerable<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null
+                    ? await context.Set<TEntity>().ToListAsync()
+                    : await context.Set<TEntity>().Where(filter).ToListAsync();
+            }
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return await context.Set<TEntity>().SingleOrDefaultAsync(filter);
+            }
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                await context.Set<TEntity>().AddAsync(entity);
+                await context.SaveChangesAsync();
+                return entity;
+            }
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                context.Set<TEntity>().Update(entity);
+                await context.SaveChangesAsync();
+                return entity;
+            }
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            using (TContext context = new TContext())
+            {
+                var deleteEntity = await context.Set<TEntity>().FindAsync(id);
+                context.Set<TEntity>().Remove(deleteEntity);
+                var data = await context.SaveChangesAsync();
+                if (data > 0)
+                    return true;
+                return false;
+            }
+        }
+    }
+}
