5fb9dd627629d865e747aa21e2279ca269e574d1
aliyasindogan <a.yasindogan@gmail.com>
2021-07-13 01:03:41 +0300

    UsersController eklendi.

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,69 @@
+﻿using Business.Abstract;
+using Entities.Dtos.UserDtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetList()
+        {
+            var result = await _userService.GetListAsync();
+            if (result != null)
+                return Ok(result);
+            return BadRequest();
+        }
+
+        [HttpGet]
+        [Route("[action]/{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _userService.GetByIdAsync(id);
+            if (result != null)
+                return Ok(result);
+            return BadRequest();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> Add([FromBody] UserAddDto userAddDto)
+        {
+            var result = await _userService.AddAsync(userAddDto);
+            if (result != null)
+                return Ok(result);
+            return BadRequest();
+        }
+
+        [HttpPut]
+        [Route("[action]")]
+        public async Task<IActionResult> Update([FromBody] UserUpdateDto userUpdateDto)
+        {
+            var result = await _userService.UpdateAsync(userUpdateDto);
+            if (result != null)
+                return Ok(result);
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        [Route("[action]/{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _userService.DeleteAsync(id);
+            if (result)
+                return Ok(true);
+            return BadRequest(false);
+        }
+    }
+}
