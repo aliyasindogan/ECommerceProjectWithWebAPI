a4b8b4f2b88a3cade8d11f9ea1000c7cff2db28b
aliyasindogan <a.yasindogan@gmail.com>
2021-07-17 16:35:25 +0300

    WinForm silindi

--- WebAPIWithWindowsForm/Properties/AssemblyInfo.cs
+++ WebAPIWithWindowsForm/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a1cbd8ca-e0b7-4263-a57d-34c352aa8713")]
+[assembly: Guid("c61cb4b7-7585-4113-80cb-f7e5447beb23")]
 
 // Version information for an assembly consists of the following four values:
 //
