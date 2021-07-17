be1307c63a62fecf216f586ac7a2e8b8363b7916
aliyasindogan <a.yasindogan@gmail.com>
2021-07-17 18:00:33 +0300

    Revert "-WinForm'un .NET Framework 4.7.2 den .NET Core 5.0'la  değiştirildi."
    This reverts commit e96d2f202ec87723ed70a270ca9d8f58fa877cea.

--- WebAPIWithWindowsForm/Form1.cs
+++ WebAPIWithWindowsForm/Form1.cs
@@ -1,28 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Json;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-using Entities.Dtos.UserDtos;
 
 namespace WebAPIWithWindowsForm
 {
     public partial class Form1 : Form
     {
-        private string url = "http://localhost:63545/api/Users/GetList";
-
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private async void Form1_Load(object sender, EventArgs e)
-        {
-            using (HttpClient httpClient = new HttpClient())
-            {
-                var users = await httpClient.GetFromJsonAsync<List<UserDetailDto>>(new Uri(url));
-                dataGridView1.DataSource = users;
-            }
         }
     }
 }
