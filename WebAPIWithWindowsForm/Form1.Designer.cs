4ed597656cae37b37f20508bc6628b1e80c48eaa
aliyasindogan <a.yasindogan@gmail.com>
2021-07-17 18:05:20 +0300

    WebAPIWithWindowsForm .NET Core 5.0 eklendi.

--- WebAPIWithWindowsForm/Form1.Designer.cs
+++ WebAPIWithWindowsForm/Form1.Designer.cs
@@ -4,12 +4,12 @@ namespace WebAPIWithWindowsForm
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -24,8 +24,8 @@ namespace WebAPIWithWindowsForm
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
--- WebAPIWithWindowsForm/Form1.cs
+++ WebAPIWithWindowsForm/Form1.cs
@@ -16,5 +16,6 @@ namespace WebAPIWithWindowsForm
         {
             InitializeComponent();
         }
+
     }
 }
