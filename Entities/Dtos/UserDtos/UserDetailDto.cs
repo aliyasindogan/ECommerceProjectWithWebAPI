eafde6c1bf093e32cf066348792f555ebea4a3db
aliyasindogan <a.yasindogan@gmail.com>
2021-07-24 23:25:09 +0300

    UserDetailDto 'da DateOfBirth property'sine [DataType(DataType.Date)] attribute eklendi.

--- Entities/Dtos/UserDtos/UserDetailDto.cs
+++ Entities/Dtos/UserDtos/UserDetailDto.cs
@@ -1,5 +1,6 @@
 ﻿using Entities.Abstract;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Entities.Dtos.UserDtos
 {
@@ -10,7 +11,10 @@ namespace Entities.Dtos.UserDtos
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Gender { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
+
         public string Email { get; set; }
         public string Address { get; set; }
     }
