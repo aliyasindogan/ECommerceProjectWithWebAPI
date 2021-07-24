c44594383704f849f65110547066d1334ed7913f
aliyasindogan <a.yasindogan@gmail.com>
2021-07-24 23:27:24 +0300

    WebAPIWithCoreMvc projesinde UsersController  eklendi. Ve Index listeleme metodu oluşturuldu.

---
+++ WebAPIWithCoreMvc/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Entities.Dtos.UserDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIWithCoreMvc.Controllers
+{
+    public class UsersController : Controller
+    {
+        #region Defines
+
+        private readonly HttpClient _httpClient;
+        private string url = "http://localhost:63545/api/";
+
+        #endregion Defines
+
+        #region Constructor
+
+        public UsersController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        #endregion Constructor
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _httpClient.GetFromJsonAsync<List<UserDetailDto>>(url + "Users/GetList");
+            return View(users);
+        }
+    }
+}
