b83dd76e6536efc321bf9462ca8df6e424c50d88
aliyasindogan <a.yasindogan@gmail.com>
2021-07-24 23:29:42 +0300

    WebAPIWithCoreMvc Startup dosyasında ConfigureServices e AddHttpClient eklendi.

--- WebAPIWithCoreMvc/Startup.cs
+++ WebAPIWithCoreMvc/Startup.cs
@@ -23,6 +23,7 @@ namespace WebAPIWithCoreMvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,7 +47,7 @@ namespace WebAPIWithCoreMvc
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Users}/{action=Index}/{id?}");
             });
         }
     }
