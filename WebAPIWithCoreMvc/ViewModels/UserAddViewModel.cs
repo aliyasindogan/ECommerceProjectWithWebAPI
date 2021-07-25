ea1e41d7b87db104f6733a1f2b69e786f4f25d56
aliyasindogan <a.yasindogan@gmail.com>
2021-07-25 16:33:11 +0300

    UserAddViewModel class'ı oluşturuldu.

---
+++ WebAPIWithCoreMvc/ViewModels/UserAddViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIWithCoreMvc.ViewModels
+{
+    public class UserAddViewModel
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public int GenderID { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
