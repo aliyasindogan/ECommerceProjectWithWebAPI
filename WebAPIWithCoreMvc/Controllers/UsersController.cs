d4741801c2a28dea260700ce30fbc3c40c60de7f
aliyasindogan <a.yasindogan@gmail.com>
2021-07-25 16:32:38 +0300

    WebAPIWithCoreMvc projesinde UsersController da HttpGet ve HttpPost olarak Add metodu oluşturuldu.Gender class'ı oluşturuldu.GenderFill() metodu oluşturuldu.

--- WebAPIWithCoreMvc/Controllers/UsersController.cs
+++ WebAPIWithCoreMvc/Controllers/UsersController.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Entities.Dtos.UserDtos;
 using Microsoft.AspNetCore.Mvc;
+using WebAPIWithCoreMvc.ViewModels;
 
 namespace WebAPIWithCoreMvc.Controllers
 {
@@ -29,6 +31,49 @@ namespace WebAPIWithCoreMvc.Controllers
         {
             var users = await _httpClient.GetFromJsonAsync<List<UserDetailDto>>(url + "Users/GetList");
             return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.GenderList = GenderFill();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(UserAddViewModel userAddViewModel)
+        {
+            UserAddDto userAddDto = new UserAddDto()
+            {
+                FirstName = userAddViewModel.FirstName,
+                Gender = userAddViewModel.GenderID == 1 ? true : false,
+                LastName = userAddViewModel.LastName,
+                Address = userAddViewModel.Address,
+                DateOfBirth = userAddViewModel.DateOfBirth,
+                Email = userAddViewModel.Email,
+                Password = userAddViewModel.Password,
+                UserName = userAddViewModel.UserName,
+            };
+            HttpResponseMessage responseMessage = await _httpClient.PostAsJsonAsync(url + "Users/Add", userAddDto);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        private List<Gender> GenderFill()
+        {
+            List<Gender> genders = new List<Gender>();
+            genders.Add(new Gender() { Id = 1, GenderName = "Erkek" });
+            genders.Add(new Gender() { Id = 2, GenderName = "Kadın" });
+            return genders;
+        }
+
+        private class Gender
+        {
+            public int Id { get; set; }
+            public string GenderName { get; set; }
         }
     }
 }
