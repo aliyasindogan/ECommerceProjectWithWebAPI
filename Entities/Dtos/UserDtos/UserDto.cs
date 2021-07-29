294fae7190ba9559ff74614581abb6475a6e7578
aliyasindogan <a.yasindogan@gmail.com>
2021-07-29 19:35:17 +0300

    UserDto class'na Password property eklendi.

--- Entities/Dtos/UserDtos/UserDto.cs
+++ Entities/Dtos/UserDtos/UserDto.cs
@@ -9,6 +9,7 @@ namespace Entities.Dtos.UserDtos
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Password { get; set; }
         public bool Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
