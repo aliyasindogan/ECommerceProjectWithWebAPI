49adb19b4c971891b219eb2fbb70bc0037e112d3
aliyasindogan <a.yasindogan@gmail.com>
2021-07-30 20:05:25 +0300

    UserDeleteViewModel oluşturuldu.

---
+++ WebAPIWithCoreMvc/ViewModels/UserDeleteViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIWithCoreMvc.ViewModels
+{
+    public class UserDeleteViewModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string GenderName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
