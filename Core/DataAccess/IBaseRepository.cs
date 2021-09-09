11a70b19b64f2576ef87957a67f32dc5dc5d717a
aliyasindogan <a.yasindogan@gmail.com>
2021-09-10 00:38:42 +0300

    Core katmanında DataAccess katmanına EntityFramework klasörü eklenip BaseRepository'ler eklendi.

---
+++ Core/DataAccess/IBaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Core.Entity.Abstract;
+
+namespace Core.DataAccess
+{
+    public interface IBaseRepository<T> where T : class, IEntity, new()
+    {
+        Task<IEnumerable<T>> GetListAsync(Expression<Func<T, bool>> filter = null);
+
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+
+        Task<T> AddAsync(T entity);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}
