b68c2387decc59c4c6af158ef9b854dd7a87e184
aliyasindogan <a.yasindogan@gmail.com>
2021-10-23 22:17:50 +0300

    UnAuthorizeException class'ı oluşturuldu.

---
+++ WebAPIWithCoreMvc/Exceptions/UnAuthorizeException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WebAPIWithCoreMvc.Exceptions
+{
+    public class UnAuthorizeException : Exception
+    {
+        public UnAuthorizeException()
+        {
+        }
+
+        public UnAuthorizeException(string message) : base(message)
+        {
+        }
+
+        public UnAuthorizeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
