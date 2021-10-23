83882b945e70046c1463059b9345d7c4f9b66528
aliyasindogan <a.yasindogan@gmail.com>
2021-10-23 22:18:17 +0300

    AuthTokenHandler (DelegatingHandler) class'ı oluşturuldu.

---
+++ WebAPIWithCoreMvc/Handler/AuthTokenHandler.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using WebAPIWithCoreMvc.Exceptions;
+
+namespace WebAPIWithCoreMvc.Handler
+{
+    public class AuthTokenHandler : DelegatingHandler
+    {
+        public AuthTokenHandler()
+        {
+        }
+
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public AuthTokenHandler(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var name = _httpContextAccessor.HttpContext.User.Identity.Name;
+            if (_httpContextAccessor.HttpContext.User.Identity.IsAuthenticated)
+            {
+                string _token = _httpContextAccessor.HttpContext.Session.GetString("token");
+                if (!String.IsNullOrEmpty(_token))
+                {
+                    request.Headers.Add("Authorization", $"Bearer {_token}");
+                }
+            }
+            var response = await base.SendAsync(request, cancellationToken);
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                throw new UnAuthorizeException();
+            }
+            return response;
+        }
+    }
+}
