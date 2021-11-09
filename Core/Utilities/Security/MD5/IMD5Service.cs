30089b8f7bc05c85afa5ba6b9d01c6102ac69f6f
aliyasindogan <a.yasindogan@gmail.com>
2021-11-09 22:28:40 +0300

    IMD5Service eklendi.

---
+++ Core/Utilities/Security/MD5/IMD5Service.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.MD5
+{
+    public interface IMD5Service
+    {
+        /// <summary>
+        /// Metni MD'e Dönüştür
+        /// </summary>
+        /// <param name="text">string text</param>
+        /// <returns>string text</returns>
+        string ConvertTextToMD5(string text);
+
+        /// <summary>
+        /// MD5 ile Şifreleme
+        /// </summary>
+        /// <param name="text">string text</param>
+        /// <returns>string text</returns>
+        string Encrypt(string text);
+
+        /// <summary>
+        /// Şifresini Çöz (MD5)
+        /// </summary>
+        /// <param name="encryptedValue">string MD5</param>
+        /// <returns>string text</returns>
+        string Decrypt(string encryptedValue);
+    }
+}
