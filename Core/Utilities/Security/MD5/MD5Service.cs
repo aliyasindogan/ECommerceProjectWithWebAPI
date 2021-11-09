d7ee743c97fbb5f37a17080dc14d9aaabffc100f
aliyasindogan <a.yasindogan@gmail.com>
2021-11-09 22:28:52 +0300

    MD5Service eklendi.

---
+++ Core/Utilities/Security/MD5/MD5Service.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.MD5
+{
+    public class MD5Service : IMD5Service
+    {
+        private string hash = "ASPDOTNETMVC09112021";
+
+        public string ConvertTextToMD5(string text)
+        {
+            MD5CryptoServiceProvider pwd = new MD5CryptoServiceProvider();
+            return EncryptionToMD5(text, pwd);
+        }
+
+        private string EncryptionToMD5(string text, HashAlgorithm alg)
+        {
+            byte[] byteValue = Encoding.UTF8.GetBytes(text);
+            byte[] encryptByte = alg.ComputeHash(byteValue);
+            return Convert.ToBase64String(encryptByte);
+        }
+
+        public string Encrypt(string text)
+        {
+            byte[] data = UTF8Encoding.UTF8.GetBytes(text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripleDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripleDes.CreateEncryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    return Convert.ToBase64String(results, 0, results.Length);
+                }
+            }
+        }
+
+        public string Decrypt(string encryptedValue)
+        {
+            byte[] data = Convert.FromBase64String(encryptedValue);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripleDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripleDes.CreateDecryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    return UTF8Encoding.UTF8.GetString(results);
+                }
+            }
+        }
+    }
+}
