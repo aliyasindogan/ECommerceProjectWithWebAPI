c087eaa76246498d9e37ac52a4c9879e89e686ac
aliyasindogan <a.yasindogan@gmail.com>
2021-11-09 22:28:17 +0300

    IHashService eklendi.

---
+++ Core/Utilities/Security/Hash/IHashService.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Utilities.Security.Hash
+{
+    public interface IHashService
+    {
+        string CreateHash(string plainText);
+
+        bool CompareHash(string hashedText, string plainText);
+    }
+}
