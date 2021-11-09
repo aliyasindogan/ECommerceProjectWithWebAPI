c3a6884bad0e22812ab56f6adac0cd2379913756
aliyasindogan <a.yasindogan@gmail.com>
2021-11-09 22:28:31 +0300

    Sha256HashService eklendi.

---
+++ Core/Utilities/Security/Hash/Sha256/Sha256HashService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Core.Utilities.Security.Hash.Sha256
+{
+    public class Sha256HashService : IHashService
+    {
+        public string CreateHash(string plainText)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(plainText));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public bool CompareHash(string hashedText, string plainText)
+        {
+            return CreateHash(plainText) == hashedText;
+        }
+    }
+}
