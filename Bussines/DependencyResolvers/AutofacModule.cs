57edd03a1212a852a289f6d00bab67d3b08c241a
aliyasindogan <a.yasindogan@gmail.com>
2021-11-21 23:46:38 +0300

    AutofacModule eklendi.

---
+++ Bussines/DependencyResolvers/AutofacModule.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.Abstract;
+using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using Core.Utilities.Security.Token;
+using Core.Utilities.Security.Token.Jwt;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace Business.DependencyResolvers
+{
+    public class AutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+            builder.RegisterType<UserService>().As<IUserService>();
+            builder.RegisterType<JwtTokenService>().As<ITokenService>();
+            builder.RegisterType<AuthService>().As<IAuthService>();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterseptorSelector()
+                }).SingleInstance();
+        }
+    }
+}
