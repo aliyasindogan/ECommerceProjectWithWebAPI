fbbf4d2dae18bbc6d3a69e3dc21159a6c6ea9614
aliyasindogan <a.yasindogan@gmail.com>
2021-11-21 23:43:45 +0300

    MethodInterception  eklendi.

---
+++ Core/Utilities/Interceptors/MethodInterception.cs
@@ -0,0 +1,60 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace Core.Utilities.Interceptors
+{
+    public abstract class MethodInterception : MethodInterceptionBaseAttribute
+    {
+        /// <summary>
+        /// Metod çalışmadan önce
+        /// </summary>
+        /// <param name="ınvocation"></param>
+        protected virtual void OnBefore(IInvocation invocation)
+        { }
+
+        /// <summary>
+        /// Metod çalıştıktan sonra
+        /// </summary>
+        /// <param name="ınvocation"></param>
+        protected virtual void OnAfter(IInvocation invocation)
+        { }
+
+        /// <summary>
+        ///  Hata (Exception) alındığında
+        /// </summary>
+        /// <param name="ınvocation"></param>
+        protected virtual void OnException(IInvocation invocation, Exception e)
+        { }
+
+        /// <summary>
+        /// İşlem başarılı olduğunda
+        /// </summary>
+        /// <param name="ınvocation"></param>
+        protected virtual void OnSuccess(IInvocation invocation)
+        { }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSuccess = true;
+            OnBefore(invocation);
+            try
+            {
+                invocation.Proceed(); //metodu çalıştır.
+            }
+            catch (Exception e)
+            {
+                isSuccess = false;
+                OnException(invocation, e);
+                throw;
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(invocation);
+                }
+            }
+            OnAfter(invocation);
+        }
+    }
+}
