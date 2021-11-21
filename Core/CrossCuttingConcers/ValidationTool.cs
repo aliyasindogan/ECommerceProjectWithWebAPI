0367a7f567192b282ea4fb5e551346176ad8eff0
aliyasindogan <a.yasindogan@gmail.com>
2021-11-21 23:45:46 +0300

    ValidationTool eklendi.

---
+++ Core/CrossCuttingConcers/ValidationTool.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Core.CrossCuttingConcers
+{
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
+            if (result.Errors.Count > 0)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
