5b916bf1b58decfd50c472ff1b69f8d48e8d3ed8
aliyasindogan <a.yasindogan@gmail.com>
2021-11-21 23:43:56 +0300

    AspectInterseptorSelector eklend

---
+++ Core/Utilities/Interceptors/AspectInterseptorSelector.cs
@@ -0,0 +1,18 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Core.Utilities.Interceptors
+{
+    public class AspectInterseptorSelector : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+        {
+            var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>(true).ToList();
+            var methodAttributes = type.GetMethod(method.Name).GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+            classAttributes.AddRange(methodAttributes);
+            return classAttributes.ToArray();
+        }
+    }
+}
