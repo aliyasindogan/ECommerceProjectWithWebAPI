03ed5171de9a9b851c9b86a100c6772e82b091a4
aliyasindogan <a.yasindogan@gmail.com>
2021-11-21 23:43:28 +0300

    MethodInterceptionBaseAttribute eklendi.

---
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace Core.Utilities.Interceptors
+{
+    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+    {
+        public virtual void Intercept(IInvocation invocation)
+        {
+        }
+    }
+}
