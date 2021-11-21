b838b8cddde120d7cea2f1adcee0108e1c2d8bfb
aliyasindogan <a.yasindogan@gmail.com>
2021-11-21 23:46:14 +0300

    LoginDtoValidator eklendi.

---
+++ Bussines/Validations/FluentValidation/LoginDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Dtos.Auth;
+using FluentValidation;
+
+namespace Business.Validations.FluentValidation
+{
+    public class LoginDtoValidator : AbstractValidator<LoginDto>
+    {
+        public LoginDtoValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Kullanıcı adı alanı boş geçilemez.");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre alanı boş geçilemez.");
+        }
+    }
+}
