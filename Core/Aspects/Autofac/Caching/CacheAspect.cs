3e42ccb472c02125929095a9d1ca889aad0573ff
Ali Yasin Doğan <a.yasindogan@gmail.com>
2022-01-10 02:22:24 +0300

    CacheAspect ve CacheRemoveAspect eklendi.

---
+++ Core/Aspects/Autofac/Caching/CacheAspect.cs
@@ -0,0 +1,33 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcers.Caching;
+using Core.Utilities.Interceptors;
+using System.Linq;
+
+namespace Core.Aspects.Autofac.Caching
+{
+    public class CacheAspect : MethodInterception
+    {
+        private readonly int _duration;
+        private readonly ICacheService _cacheService;
+
+        public CacheAspect(int duration = 60)
+        {
+            _duration = duration;
+            _cacheService = (ICacheService)Utilities.Helpers.HttpContext.Current.RequestServices.GetService(typeof(ICacheService));
+        }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            var methodName = string.Format($"{invocation.Method.ReflectedType.FullName}.{invocation.Method.Name}");
+            var arguments = invocation.Arguments.ToList();
+            var key = $"{methodName}({string.Join(",", arguments.Select(x => x?.ToString() ?? "<Null>"))})";
+            if (_cacheService.IsAdd(key))
+            {
+                invocation.ReturnValue = _cacheService.Get(key);
+                return;
+            }
+            invocation.Proceed();
+            _cacheService.Add(key, invocation.ReturnValue, _duration);
+        }
+    }
+}
---
+++ Core/Aspects/Autofac/Caching/CacheRemove.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Castle.DynamicProxy;
+using Core.CrossCuttingConcers.Caching;
+using Core.Utilities.Interceptors;
+
+namespace Core.Aspects.Autofac.Caching
+{
+    public class CacheRemoveAspect : MethodInterception
+    {
+        private  string _methodName;
+        private readonly ICacheService _cacheService;
+
+        public CacheRemoveAspect(string methodName)
+        {
+            _methodName = methodName;
+            _cacheService = (ICacheService)Utilities.Helpers.HttpContext.Current.RequestServices.GetService(typeof(ICacheService));
+        }
+
+        protected override void OnSuccess(IInvocation invocation)
+        {
+            //Todo: 10.01.2022  CacheRemoveAspect  Remove'da düzenleme yapılacak.
+            _cacheService.Remove($"Business.Abstract.{_methodName}()");
+        }
+    }
+}
