14f3ca4f94bdf22bc7e32fe83029952d9501a2e6
Ali Yasin Doğan <a.yasindogan@gmail.com>
2022-01-10 02:23:59 +0300

    Caching e MemoryCacheService ve ICacheService eklendi.

---
+++ Core/CrossCuttingConcers/Caching/ICacheService.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.CrossCuttingConcers.Caching
+{
+    public interface ICacheService
+    {
+        T Get<T>(string key);
+
+        object Get(string key);
+
+        void Add(string key, object data, int duration);
+
+        bool IsAdd(string key);
+
+        void Remove(string key);
+       
+    }
+}
---
+++ Core/CrossCuttingConcers/Caching/Microsoft/MemoryCacheService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+
+namespace Core.CrossCuttingConcers.Caching.Microsoft
+{
+    public class MemoryCacheService : ICacheService
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public MemoryCacheService()
+        {
+            _memoryCache =
+                (IMemoryCache)Utilities.Helpers.HttpContext.Current.RequestServices.GetService(typeof(IMemoryCache));
+        }
+
+        public T Get<T>(string key)
+        {
+            return _memoryCache.Get<T>(key);
+        }
+
+        public object Get(string key)
+        {
+            return _memoryCache.Get(key);
+        }
+
+        public void Add(string key, object data, int duration)
+        {
+            _memoryCache.Set(key, data, TimeSpan.FromMinutes(duration));
+        }
+
+        public bool IsAdd(string key)
+        {
+            return _memoryCache.TryGetValue(key, out _);
+        }
+
+        public void Remove(string key)
+        {
+            _memoryCache.Remove(key);
+        }
+
+    }
+}
