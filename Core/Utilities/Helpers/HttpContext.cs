24e3ace464a56c487957add5731e832f99cccfb4
Ali Yasin Doğan <a.yasindogan@gmail.com>
2022-01-09 23:50:54 +0300

    HttpContext eklendi.

---
+++ Core/Utilities/Helpers/HttpContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Core.Utilities.Helpers
+{
+    public static class HttpContext
+   {
+       private static IHttpContextAccessor _httpContextAccessor;
+       public static Microsoft.AspNetCore.Http.HttpContext Current
+       {
+           get { return _httpContextAccessor.HttpContext; }
+       }
+
+       internal static void Configure(IHttpContextAccessor httpContextAccessor)
+       {
+           _httpContextAccessor = httpContextAccessor;
+       }
+   }
+}
