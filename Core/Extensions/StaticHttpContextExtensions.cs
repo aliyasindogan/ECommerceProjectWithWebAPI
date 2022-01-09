b4f1fefe631c776c7aa7ac99bd41e57edb3bb607
Ali Yasin Doğan <a.yasindogan@gmail.com>
2022-01-09 23:51:11 +0300

    StaticHttpContextExtensions eklendi.

---
+++ Core/Extensions/StaticHttpContextExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Extensions
+{
+    public static class StaticHttpContextExtensions
+    {
+        public static void AddCustomHttpContextAccessor(this IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        }
+
+        public static IApplicationBuilder UseStaticHttpContext(this IApplicationBuilder app)
+        {
+            var httpContextAccessor = app.ApplicationServices.GetRequiredService<IHttpContextAccessor>();
+            Utilities.Helpers.HttpContext.Configure(httpContextAccessor);
+            return app;
+        }
+    }
+}
