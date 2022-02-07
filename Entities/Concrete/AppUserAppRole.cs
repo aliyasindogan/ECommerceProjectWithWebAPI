7fb7cbbf9eaf16b954cde6ecc4e00701adc19da2
Ali Yasin Doğan <a.yasindogan@gmail.com>
2022-02-08 00:32:59 +0300

    Entities.Concrete AppUserAppRole class ı eklendi.

---
+++ Entities/Concrete/AppUserAppRole.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Core.Entities.Concrete;
+
+namespace Entities.Concrete
+{
+    public class AppUserAppRole : UserRole
+    {
+        public string RoleStatus { get; set; }
+        [ForeignKey("RoleId")]
+        public virtual AppRole AppRole { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual AppUser AppUser { get; set; }
+    }
+}
