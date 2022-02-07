21ff97c179bf34b4f7ab14d504403544aee2a37e
Ali Yasin Doğan <a.yasindogan@gmail.com>
2022-02-08 00:31:55 +0300

    Entities.Concrete AppUser class ı eklendi.

---
+++ Entities/Concrete/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace Entities.Concrete
+{
+    public class AppUser : User
+    {
+        public AppUser()
+        {
+            AppUserAppRoles = new HashSet<AppUserAppRole>();
+        }
+        public Guid RefreshToken { get; set; }
+        public virtual ICollection<AppUserAppRole> AppUserAppRoles { get; set; }
+    }
+}
