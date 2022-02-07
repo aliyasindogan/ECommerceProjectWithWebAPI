73f32e99c1af3d41f61d6f237708851aec09797a
Ali Yasin Doğan <a.yasindogan@gmail.com>
2022-02-08 00:32:28 +0300

    Entities.Concrete klasörüne AppRole class ı eklendi.

---
+++ Entities/Concrete/AppRole.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Core.Entities.Concrete;
+
+namespace Entities.Concrete
+{
+    public class AppRole : Role
+    {
+        public AppRole()
+        {
+            AppUserAppRoles = new HashSet<AppUserAppRole>();
+        }
+        public virtual ICollection<AppUserAppRole> AppUserAppRoles { get; set; }
+    }
+}
