f1dd3f8b6546928503e8e33fee0f1feb90963b5f
Ali Yasin Doğan <a.yasindogan@gmail.com>
2022-03-05 12:23:10 +0300

    DataAccess de IAppUserDal ve EfAppUserDal olarak isimleri değiştirildi.

--- DataAccess/Abstract/IUserDal.cs
+++
@@ -1,9 +0,0 @@
-﻿using Core.DataAccess;
-using Entities.Concrete;
-
-namespace DataAccess.Abstract
-{
-    public interface IUserDal : IBaseRepository<User>
-    {
-    }
-}
--- DataAccess/Concrete/EntityFramework/EfAppUserDal.cs
+++ DataAccess/Concrete/EntityFramework/EfAppUserDal.cs
@@ -1,11 +1,11 @@
 ﻿using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete.Contexts;
-using Entities.Concrete;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfUserDal : EfBaseRepository<User, ECommerceProjectWithWebAPIContext>, IUserDal
+    public class EfAppUserDal : EfBaseRepository<User, ECommerceDbContext>, IAppUserDal
     {
     }
 }
