eb75c58f829891275b17f98923e190d3cb177e45
Ali Yasin Doğan <a.yasindogan@gmail.com>
2022-03-06 01:30:21 +0300

    AppOperationClaimDto eklendi.

---
+++ Entities/Dtos/AppOperationClaim/AppOperationClaimDto.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities;
+
+namespace Entities.Dtos.AppOperationClaim
+{
+    public class AppOperationClaimDto : IDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
