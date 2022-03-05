d9c928cb7ad647d8da4374eaa07396cb098b7889
Ali Yasin Doğan <a.yasindogan@gmail.com>
2022-03-06 01:29:11 +0300

    IAppUserDal ve AppUserDal'a GetRolesAsync metodu eklendi.

--- DataAccess/Abstract/IAppUserDal.cs
+++ DataAccess/Abstract/IAppUserDal.cs
@@ -1,9 +1,15 @@
 ﻿using Core.DataAccess;
+using Core.Entities.Concrete;
+using Core.Entities.Dtos;
 using Entities.Concrete;
+using Entities.Dtos.AppOperationClaim;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
     public interface IAppUserDal : IBaseRepository<AppUser>
     {
+        Task<List<OperationClaimDto>> GetRolesAsync(User user);
     }
 }
--- DataAccess/Concrete/EntityFramework/EfAppUserDal.cs
+++ DataAccess/Concrete/EntityFramework/EfAppUserDal.cs
@@ -1,11 +1,34 @@
 ﻿using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
+using Core.Entities.Dtos;
 using DataAccess.Abstract;
 using DataAccess.Concrete.Contexts;
 using Entities.Concrete;
+using Entities.Dtos.AppOperationClaim;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
     public class EfAppUserDal : EfBaseRepository<AppUser, ECommerceDbContext>, IAppUserDal
     {
+        public async Task<List<OperationClaimDto>> GetRolesAsync(User user)
+        {
+            using (var context = new ECommerceDbContext())
+            {
+                var result = from appUserTypeAppOperationClaim in context.AppUserTypeAppOperationClaims
+                             join appOperationClaim in context.AppOperationClaims on appUserTypeAppOperationClaim.OperationClaimId equals appOperationClaim.Id
+                             join appUserType in context.AppUserTypes on appUserTypeAppOperationClaim.UserTypeId equals appUserType.Id
+                             where appUserTypeAppOperationClaim.UserTypeId == user.UserTypeId
+                             select new OperationClaimDto
+                             {
+                                 Id = appOperationClaim.Id,
+                                 Name = appOperationClaim.Name
+                             };
+                return await result.ToListAsync();
+            }
+        }
     }
 }
