0ecf894ccddd62b45f1b26ddc88860158c8e6293
Ali Yasin Doğan <a.yasindogan@gmail.com>
2022-03-05 21:27:03 +0300

    UserOperationClaim class'ında UserId UserTypeId olarak düzenlendi.

--- Core/Entities/Concrete/UserOperationClaim.cs
+++ Core/Entities/Concrete/UserOperationClaim.cs
@@ -5,7 +5,7 @@ namespace Core.Entities.Concrete
 {
     public class UserOperationClaim : BaseEntity, IUpdatedEntity
     {
-        public int UserId { get; set; }
+        public int UserTypeId { get; set; }
         public int OperationClaimId { get; set; }
         public int? UpdatedUserId { get; set; }
         public DateTime? UpdatedDate { get; set; }
