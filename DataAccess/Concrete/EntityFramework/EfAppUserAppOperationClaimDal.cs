fd6f939c4042892a70bb6d401f8d6fc6afc8c4bd
Ali Yasin Doğan <a.yasindogan@gmail.com>
2022-03-05 21:30:53 +0300

    EfAppOperationClaimDal,EfAppUserTypeAppOperationClaimDal ve EfAppUserTypeDal eklendi.

---
+++ DataAccess/Concrete/EntityFramework/EfAppUserAppOperationClaimDal.cs
@@ -0,0 +1,11 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.Contexts;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfAppUserTypeAppOperationClaimDal : EfBaseRepository<AppUserTypeAppOperationClaim, ECommerceDbContext>, IAppUserTypeAppOperationClaimDal
+    {
+    }
+}
