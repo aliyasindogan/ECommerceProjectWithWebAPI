d1e56f58403e23915ce7fa7deef0621a7a0d2503
Ali Yasin Doğan <a.yasindogan@gmail.com>
2022-03-06 18:45:58 +0300

    RegisterDto eklendi.

---
+++ Entities/Dtos/Auth/RegisterDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities.Dtos.Auth
+{
+    public  class RegisterDto
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string ProfileImageUrl { get; set; }
+        public string GsmNumber { get; set; }
+    }
+}
