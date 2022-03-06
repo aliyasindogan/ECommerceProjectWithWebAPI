da81ade1fa84c8105aada4ced9348d3401a9d58c
Ali Yasin Doğan <a.yasindogan@gmail.com>
2022-03-06 18:44:19 +0300

    Messages'a NotAdded,UserNameAlreadyExist ve UserRegistered mesajları eklendi.

--- Bussines/Constants/Messages.cs
+++ Bussines/Constants/Messages.cs
@@ -7,8 +7,11 @@
         public const string Updated = "Güncellendi";
         public const string Listed = "Listelendi";
         public const string NotListed = "Listelenmedi";
+        public const string NotAdded = "Eklenmedi";
         public const string UserNotFound = "Kullanıcı bulunamadı";
         public const string SystemLoginSuccessful = "Sisteme giriş başarılı";
         public const string SystemLoginFailed = "Sisteme giriş başarısız";
+        public const string UserNameAlreadyExist = "Bu kullanıcı zaten mevcut";
+        public const string UserRegistered = "Kullanıcı başarıyla kaydedildi.";
     }
 }
